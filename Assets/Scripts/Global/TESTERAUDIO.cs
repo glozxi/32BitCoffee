573c4ff3d2df07444d903e4125f59806f2c5eff1
9fc70c892 <89006592+9fc70c892@users.noreply.github.com>
2022-06-20 00:37:41 +0800

    Purpose is to change Brew to use Image for Character
    Also to change VM parts to use the newer one. Removed in last merger to prevent deletion of serveral new updates to scene.
    
    INCOMPLETE

--- Assets/Scripts/Global/TESTERAUDIO.cs
+++ Assets/Scripts/Global/TESTERAUDIO.cs
@@ -16,10 +16,7 @@ public class TESTERAUDIO : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            // CharacterManager.instance.CMChar("PERSON2", "person_pose", "happy", "MM");
-            // CharacterManager.instance.CMChar("PERSON3", "person_default", "shocked", "MR");
-            AudioManager.instance.PlaySFX("buzzer", 1, 1);
-            AudioManager.instance.PlayBGM("menu", 1,1,1);
+            CharacterManager.instance.CMChar("PERSON2", "person_default", "default", "LL");
             BackgroundManager.instance.CMChangeBackground("day");
         }
     }
