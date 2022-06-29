33580ebc2f45bc28e0100e358b878db7b990dbc0
glozxi <glozxi@gmail.com>
2022-06-29 19:52:22 +0800

    Fixed size of save image problem

--- Assets/Scripts/Saves Scene/SavePrefab.cs
+++ Assets/Scripts/Saves Scene/SavePrefab.cs
@@ -29,9 +29,7 @@ public class SavePrefab : MonoBehaviour
         string picPath = Path.Split('.')[0] + ".png";
         byte[] bytes = File.ReadAllBytes(picPath);
         thisTexture.LoadImage(bytes);
-        thisTexture.name = picPath;
         _rawImage.texture = thisTexture;
-        SizeToParent();
     }
 
     public void LoadGame()
@@ -39,28 +37,5 @@ public class SavePrefab : MonoBehaviour
         _gameStateManager.LoadGame(Path);
 
     }
-    public void SizeToParent(float padding = 0)
-    {
-        var parent = _rawImage.transform.parent.GetComponent<RectTransform>();
-        var imageTransform = _rawImage.GetComponent<RectTransform>();
-        if (!parent) { return; } //if we don't have a parent, just return our current width;
-        padding = 1 - padding;
-        float w = 0, h = 0;
-        float ratio = _rawImage.texture.width / (float)_rawImage.texture.height;
-        var bounds = new Rect(0, 0, parent.rect.width, parent.rect.height);
-        if (Mathf.RoundToInt(imageTransform.eulerAngles.z) % 180 == 90)
-        {
-            //Invert the bounds if the image is rotated
-            bounds.size = new Vector2(bounds.height, bounds.width);
-        }
-        //Size by height first
-        h = bounds.height * padding;
-        w = h * ratio;
-        if (w > bounds.width * padding)
-        { //If it doesn't fit, fallback to width;
-            w = bounds.width * padding;
-            h = w / ratio;
-        }
-        imageTransform.sizeDelta = new Vector2(w, h);
-    }
+    
 }
