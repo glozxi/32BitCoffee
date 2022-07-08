e5ab745acb1907ad7d6ed29484b8862eaaaa161b
glozxi <glozxi@gmail.com>
2022-07-08 15:23:07 +0800

    Turn brew time bar to UI slider

--- Assets/Scripts/Brewing/Customer Scripts/CountdownBar.cs
+++ Assets/Scripts/Brewing/Customer Scripts/CountdownBar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CountdownBar : MonoBehaviour
 {
@@ -10,9 +11,29 @@ public class CountdownBar : MonoBehaviour
     [SerializeField]
     private GameObject _textObject;
 
+    [SerializeField]
+    private Slider _slider;
+
+    private bool _isActive = true;
+
     private void Update()
     {
-        _textObject.SetActive(!(_timer.GetRatioOfBonusTimeRemaining() <= 0));
-        transform.localScale = new Vector2(_timer.GetRatioOfBonusTimeRemaining(), 1);
+        // is active but shouldnt be, or not active but should be
+        if (_isActive == _timer.GetRatioOfBonusTimeRemaining() <= 0)
+        {
+            SetActiveAllChildren(transform, !(_timer.GetRatioOfBonusTimeRemaining() <= 0));
+            _isActive = !(_timer.GetRatioOfBonusTimeRemaining() <= 0);
+        }
+        _slider.value = _timer.GetRatioOfBonusTimeRemaining();
+    }
+
+    private void SetActiveAllChildren(Transform transform, bool value)
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(value);
+
+            SetActiveAllChildren(child, value);
+        }
     }
 }
